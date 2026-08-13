869f3d22a7b190bdad8908b49d5322577c121a81
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-08-14 08:15:45 +0900

    Fix local user parameters checkstyle

--- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddLocalUserParameters.java
+++ backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/action/AddLocalUserParameters.java
@@ -19,14 +19,43 @@ public class AddLocalUserParameters extends ActionParametersBase {
         this.passwordValidTo = passwordValidTo;
     }
 
-    public String getUserName() { return userName; }
-    public void setUserName(String userName) { this.userName = userName; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getPasswordValidTo() { return passwordValidTo; }
-    public void setPasswordValidTo(String passwordValidTo) { this.passwordValidTo = passwordValidTo; }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordValidTo() {
+        return passwordValidTo;
+    }
+
+    public void setPasswordValidTo(String passwordValidTo) {
+        this.passwordValidTo = passwordValidTo;
+    }
 }
