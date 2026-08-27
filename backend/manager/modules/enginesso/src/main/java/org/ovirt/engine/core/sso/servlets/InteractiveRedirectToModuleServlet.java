55309f95103a5b2cd329503235b15d7e94c47378
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-08-28 08:12:16 +0900

    Suppress stale errors on initial SSO login

--- backend/manager/modules/enginesso/src/main/java/org/ovirt/engine/core/sso/servlets/InteractiveRedirectToModuleServlet.java
+++ backend/manager/modules/enginesso/src/main/java/org/ovirt/engine/core/sso/servlets/InteractiveRedirectToModuleServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.ovirt.engine.core.sso.api.SsoConstants;
+import org.ovirt.engine.core.sso.api.SsoSession;
 import org.ovirt.engine.core.sso.service.SsoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,8 +26,17 @@ public class InteractiveRedirectToModuleServlet extends HttpServlet {
                     request,
                     response);
         } else {
-            SsoService.getSsoSession(request).setReauthenticate(false);
+            prepareInitialLoginForm(SsoService.getSsoSession(request));
             response.sendRedirect(request.getContextPath() + SsoConstants.INTERACTIVE_LOGIN_FORM_URI);
         }
+    }
+
+    static void prepareInitialLoginForm(SsoSession ssoSession) {
+        // This path opens a new interactive login form; it is not the redirect used after a
+        // submitted credential failure. Do not carry a stale error from an earlier SSO flow
+        // into the first WebAdmin login screen.
+        ssoSession.setLoginMessage(""); //$NON-NLS-1$
+        ssoSession.setLoginErrorCode(""); //$NON-NLS-1$
+        ssoSession.setReauthenticate(false);
     }
 }
