95e71339026051fdbe3db364d4b9c27e8c9a5931
mussosinsa <62009100+mussosinsa@users.noreply.github.com>
2026-08-31 09:28:31 +0900

    webadmin: remove user session duration setting

--- backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UserEnvironmentVariableCommandBase.java
+++ backend/manager/modules/bll/src/main/java/org/ovirt/engine/core/bll/UserEnvironmentVariableCommandBase.java
@@ -2,9 +2,7 @@ package org.ovirt.engine.core.bll;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -18,9 +16,8 @@ import org.ovirt.engine.core.compat.Guid;
 
 abstract class UserEnvironmentVariableCommandBase<T extends EngineConfigValueParameters> extends CommandBase<T> {
 
-    private static final Set<String> SUPPORTED_KEYS = new HashSet<>(Arrays.asList(
-            "MAX_LOGIN_MINUTES", //$NON-NLS-1$
-            "MAX_FAILURES_SINCE_SUCCESS")); //$NON-NLS-1$
+    private static final Set<String> SUPPORTED_KEYS = Collections.singleton(
+            "MAX_FAILURES_SINCE_SUCCESS"); //$NON-NLS-1$
 
     UserEnvironmentVariableCommandBase(T parameters, CommandContext cmdContext) {
         super(parameters, cmdContext);
